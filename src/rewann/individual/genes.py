Implemented edge signs and fixed some minor issue (#51)

src/rewann/individual/genes.py
@@ -15,16 +15,31 @@ class Genotype:
     n_out : int
 
     edge_encoding : GeneEncoding = (
-        ('id', np.dtype(int)), # innovation number
-        ('src', np.dtype(int)), # node id
-        ('dest', np.dtype(int)), # node id
-    #    ('weight', np.dtype(int)), # WANN for now
-        ('enabled', np.dtype(bool)), # node id
+        # innovation number
+        ('id', np.dtype(int)),
+
+         # id of source node (any but output)
+        ('src', np.dtype(int)),
+
+        # id of destination node (either hidden or output)
+        ('dest', np.dtype(int)),
+
+        # in {-1,+1} if negative signs allowed, else 1
+        ('sign', np.dtype(int)),
+
+        # sign needs to be retained even if disabled, in case the edge is
+        # reenabled
+        ('enabled', np.dtype(bool))
     )
     node_encoding : GeneEncoding = (
         ('id', np.dtype(int)),
-        ('out', np.dtype(bool)), # don't store ins and bias as genes
-        ('func', np.dtype(int)) # activation function
+
+        # input and bias nodes are not stored in genes, since no activation
+        # function is required (ids are still reserved for these nodes)
+        ('out', np.dtype(bool)),
+
+        # int representation of activation function
+        ('func', np.dtype(int))
     )
 
     def node_out_factory(self, id):
@@ -66,7 +81,7 @@ class Genotype:
 
     def __eq__(self, other):
         return (
-                self.n_in == other.n_in
+            self.n_in == other.n_in
             and self.n_out == other.n_out
             and np.all(self.edges == other.edges)
             and np.all(self.nodes == other.nodes)
@@ -88,16 +103,25 @@ class Genotype:
         return cls(edges=edges, nodes=nodes, n_in=n_in, n_out=n_out)
 
     @classmethod
-    def full_initial(cls, n_in, n_out, prob_enabled=1):
+    def full_initial(cls, n_in, n_out, prob_enabled=1, negative_edges_allowed=False):
         """Create new base gene with all input nodes connected to the output nodes."""
         # connect all input (and bias) nodes to all output nodes
         n_edges = (n_in+1)*n_out
 
         edges = np.zeros(n_edges, dtype=list(cls.edge_encoding))
-        edges['id'] = np.arange(n_edges)
+
+        edges['id'] = 0 # initial edges dont keep an id
+
         edges['src'] = np.tile(np.arange(n_in+1), n_out)
         edges['dest'] = np.repeat(n_in+1 + np.arange(n_out), n_in+1)
+
         edges['enabled'] = np.random.rand(n_edges) < prob_enabled
+
+        if negative_edges_allowed:
+            edges['sign'] = np.random.choice([-1,+1], n_edges)
+        else:
+            edges['sign'] = 1
+
 
         # start only with output nodes (input and bias nodes are implicit)
         nodes = np.zeros(n_out, dtype=list(cls.node_encoding))

src/rewann/individual/genetic_operations.py
@@ -3,7 +3,7 @@ import logging
 
 def add_node(ind, env, innov):
     # Choose an enabled edge
-    options, = np.where(ind.genes.edges['enabled'])
+    options, = np.where(ind.genes.edges['sign'].astype(bool))
     if len(options) == 0: return None
 
     edge_to_disable = np.random.choice(options)
@@ -14,7 +14,13 @@ def add_node(ind, env, innov):
     new_node['func'] = np.random.randint(ind.network.n_act_funcs)
 
     new_edges = np.zeros(2, dtype=ind.genes.edges.dtype)
+
     new_edges['enabled'] = True
+
+    # first edge will have same sign as original edge, the second one will be 1
+    # (so the total factor will stay the same if act func is linear)
+    new_edges[0]['sign'] = ind.genes.edges[edge_to_disable]['sign']
+    new_edges[1]['sign'] = 1
 
     # edge from src to new node
     new_edges[0]['src'] = ind.genes.edges[edge_to_disable]['src']
@@ -35,6 +41,19 @@ def add_node(ind, env, innov):
         edges=edges, nodes=np.append(ind.genes.nodes, new_node),
         n_in=ind.genes.n_in, n_out=ind.genes.n_out
     )
+
+def new_edge(env, innov, ind, src, dest):
+    e = np.zeros(1, dtype=ind.genes.edges.dtype)
+    e['src'] = src
+    e['dest'] = dest
+    e['enabled'] = True
+    if env['population']['enable_edge_signs']:
+        e['sign'] = np.random.choice([-1,1])
+    else:
+        e['sign'] = 1
+
+    e['id'] = innov.next_edge_id()
+    return e
 
 def add_edge_layer_based(ind, env, innov):
     """Layer-based edge introduction strategy.
@@ -73,15 +92,10 @@ def add_edge_layer_based(ind, env, innov):
 
             dest = ind.network.nodes['id'][dest - ind.network.offset]
 
-            # add edge
-            new_edge = np.zeros(1, dtype=ind.genes.edges.dtype)
-            new_edge['src'] = src
-            new_edge['dest'] = dest
-            new_edge['enabled'] = True
-            new_edge['id'] = innov.next_edge_id()
 
             return ind.genes.__class__(
-                edges=np.append(ind.genes.edges, new_edge), nodes=np.copy(ind.genes.nodes),
+                edges=np.append(ind.genes.edges, new_edge(env, innov, ind, src, dest)),
+                nodes=ind.genes.nodes,
                 n_in=ind.genes.n_in, n_out=ind.genes.n_out
             )
 
@@ -129,14 +143,9 @@ def add_edge_layer_agnostic(ind, env, innov):
 
     dest = network.nodes['id'][dest]
 
-    new_edge = np.zeros(1, dtype=ind.genes.edges.dtype)
-    new_edge['src'] = src
-    new_edge['dest'] = dest
-    new_edge['enabled'] = True
-    new_edge['id'] = innov.next_edge_id()
-
     return ind.genes.__class__(
-        edges=np.append(ind.genes.edges, new_edge), nodes=np.copy(ind.genes.nodes),
+        edges=np.append(ind.genes.edges, new_edge(env, innov, ind, src, dest)),
+        nodes=ind.genes.nodes,
         n_in=ind.genes.n_in, n_out=ind.genes.n_out
     )
 
@@ -148,7 +157,6 @@ def add_edge(ind, env, innov):
 
 def reenable_edge(ind, env, innov):
     edges = np.copy(ind.genes.edges)
-    nodes = np.copy(ind.genes.nodes)
 
     # Choose an disabled edge
     options, = np.where(edges['enabled'] == False)
@@ -159,13 +167,12 @@ def reenable_edge(ind, env, innov):
     edges[edge_to_enable]['enabled'] = True
 
     return ind.genes.__class__(
-        edges=edges, nodes=nodes,
+        edges=edges, nodes=ind.genes.nodes,
         n_in=ind.genes.n_in, n_out=ind.genes.n_out
     )
 
 def change_activation(ind, env, innov):
     nodes = np.copy(ind.genes.nodes)
-    edges = np.copy(ind.genes.edges)
 
     selected_node = np.random.randint(len(nodes))
 
@@ -176,7 +183,22 @@ def change_activation(ind, env, innov):
     nodes[selected_node]['func'] = new_act
 
     return ind.genes.__class__(
-        edges=edges, nodes=nodes,
+        edges=ind.genes.edges, nodes=nodes,
+        n_in=ind.genes.n_in, n_out=ind.genes.n_out
+    )
+
+def change_edge_sign(ind, env, innov):
+    edges = np.copy(ind.genes.edges)
+    # Choose an enabled edge
+    options, = np.where(ind.genes.edges['sign'].astype(bool))
+    if len(options) == 0: return None
+
+    edge_to_change = np.random.choice(options)
+
+    edges['sign'][edge_to_change] = -1
+
+    return ind.genes.__class__(
+        edges=edges, nodes=ind.genes.nodes,
         n_in=ind.genes.n_in, n_out=ind.genes.n_out
     )
 
@@ -187,13 +209,20 @@ def mutation(ind, env, innov):
         ('new_node', add_node),
         ('reenable_edge', reenable_edge),
         ('change_activation', change_activation),
+        ('change_edge_sign', change_edge_sign) # might be disabled
     ], dtype=[('name', 'U32'), ('func', object)])
 
     # get probabilities from params
     probabilities = np.array([
-        env['mutation', name, 'propability']
+        env['mutation', name, 'probability']
         for name in supported_mutation_types['name']
     ])
+
+    num_choices = len(supported_mutation_types)
+
+    if not env['population']['enable_edge_signs']:
+        probabilities[-1] = 0
+        num_choices -= 1
 
     # normalize
     probabilities = probabilities / np.sum(probabilities)
@@ -201,7 +230,7 @@ def mutation(ind, env, innov):
     # Generate permutation of functions (every function occurs only once)
 
     permutated_mutation_functions = np.random.choice(
-        supported_mutation_types, len(supported_mutation_types),
+        supported_mutation_types, num_choices,
         p=probabilities, replace=False)
 
     # apply functions until one actually generates a mutation

src/rewann/postopt/vis/vis_network.py
@@ -87,13 +87,18 @@ def draw_graph(net, ax=None, pos_iterations=None, layer_h=17):
 
     pos = dict(zip(nodes, np.array([pos['x'], pos['y']]).T))
 
+    edge_colors = list()
 
     # Edges
     for row, col in zip(*np.where(net.weight_matrix != 0)):
         g.add_edge(nodes[row], nodes[col + net.offset])
+        edge_colors.append(
+            'k' if net.weight_matrix[row][col] > 0
+            else 'r'
+        )
 
     nx.draw(
-        g, ax=ax, pos=pos, node_color=color, edge_color='k',
+        g, ax=ax, pos=pos, node_color=color, edge_color=edge_colors,
         with_labels=False, node_size=50, width=.4,
         arrows=True, linewidths=0, alpha=.9, arrowstyle='-')
 

tests/test_individual.py
@@ -15,17 +15,17 @@ sample = Individual(genes=Individual.Genotype(
         (12, False, 0),
     ],
     edges=[
-        # innovation id, src, dest, enabled
-        ( 3, 12,  7, True), # not necessarily ordered
-        ( 1,  1, 12, True),
-        ( 0,  0, 12, True),
-        ( 2, 12,  4, True),
-        ( 4,  7,  4, True),
-        (15,  2,  7, True),
-        ( 6,  7, 10, True),
-        ( 7, 10,  5, True),
-        ( 8,  2, 10, False), # disabled
-        (11,  3, 10, True),
+        # innovation id, src, dest, sign, enabled
+        ( 3, 12,  7, 1, True), # not necessarily ordered
+        ( 1,  1, 12,-1, True),
+        ( 0,  0, 12, 1, True),
+        ( 2, 12,  4, 1, True),
+        ( 4,  7,  4, 1, True),
+        (15,  2,  7, 1, True),
+        ( 6,  7, 10, 1, True),
+        ( 7, 10,  5,-1, True),
+        ( 8,  2, 10, 1, False), # disabled
+        (11,  3, 10,-1, True),
     ]
 ))
 
@@ -33,15 +33,18 @@ def test_gene_expression():
     sample.express()
 
     expected_weight_matrix = np.array([
-        # Inputs + Bias
-        [1, 0, 0, 0, 0],
-        [1, 0, 0, 0, 0],
-        [0, 1, 0, 0, 0],
-        [0, 0, 1, 0, 0],
+        # Inputs + Bias      (ids)
+        [ 1, 0, 0, 0, 0 ],  #  0
+        [-1, 0, 0, 0, 0 ],  #  1
+        [ 0, 1, 0, 0, 0 ],  #  2
+        [ 0, 0,-1, 0, 0 ],  #  3
         # Hidden nodes
-        [0, 1, 0, 1, 0],
-        [0, 0, 1, 1, 0],
-        [0, 0, 0, 0, 1]
+        [ 0, 1, 0, 1, 0 ],  # 12
+        [ 0, 0, 1, 1, 0 ],  #  7
+        [ 0, 0, 0, 0,-1 ],  # 10
+
+        #12  7 10  4  5 (ids)
+
     ], dtype=np.float)
 
 